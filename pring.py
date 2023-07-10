fca05ea8a3ad61adbe2616cd66272d1fd908d5ea
vakobabajanashvili <138526048+vakobabajanashvili@users.noreply.github.com>
2023-07-10 19:42:03 +0400

    Add files via upload

---
+++ pring.py
@@ -0,0 +1,15 @@
+name = "vako"
+
+
+surname = "babajanashvili"
+
+print(name)
+
+name = "vako" #str (string) ტიპის ცვლადი
+age = 16 #int (integer) მთელი რიცხვი
+height = 176 #float ათწილადი
+
+working_out = True
+very_strong = False
+
+print(name +" "+ surname)
